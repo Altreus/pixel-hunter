4e06e4ea37e5816a51633ead23cee31ad5baebc4
Alastair Douglas (altreus) <altreus@altre.us>
2020-04-23 22:49:57 +0100

    New level on click

--- game.js
+++ game.js
@@ -1,23 +1,63 @@
 var canvas = document.getElementById('game-canvas');
-var ctx = canvas.getContext('2d');
 canvas.style.background = "#03fafa";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-var pixel = [getRandomInt(canvas.width), getRandomInt(canvas.height)];
+// resolution: number of pixels per pixel
+// we truncate, so you end up with a deadzone at the right and/or bottom border.
+function makeGame(canvas, resolution) {
+  var ctx = canvas.getContext('2d');
+  canvas.style.cursor = "default";
 
-ctx.fillText(pixel[0] + " " + pixel[1], 10, 30);
+  var pixel;
 
-canvas.addEventListener('mousemove', function(e) {
-  ctx.clearRect(0,0,100,15);
-  ctx.fillText(e.offsetX + " " + e.offsetY, 10, 10);
+  makeLevel();
 
-  if (e.offsetX == pixel[0] && e.offsetY == pixel[1]) {
-    canvas.style.cursor = "pointer";
-  }
-  else {
+  function makeLevel() {
     canvas.style.cursor = "default";
+    pixel = newPixel();
+    debugPixel();
   }
-});
+  function newPixel() {
+    return [getRandomInt(canvas.width / resolution), getRandomInt(canvas.height / resolution)];
+  }
+
+  function debugPixel() {
+    ctx.clearRect(0,15,100,15);
+    ctx.fillText(pixel[0] + " " + pixel[1], 10, 30);
+  }
+  function debugEvent(e) {
+    ctx.clearRect(0,0,100,15);
+    var adjustedX = Math.floor(e.offsetX / resolution);
+    var adjustedY = Math.floor(e.offsetY / resolution);
+    ctx.fillText(adjustedX + " " + adjustedY, 10, 10);
+  }
+  function thisIsThePixel(x,y) {
+    var adjustedX = Math.floor(x / resolution);
+    var adjustedY = Math.floor(y / resolution);
+    return adjustedX == pixel[0] && adjustedY == pixel[1];
+  }
+
+  function changeCursor(e) {
+    debugEvent(e);
+    if (thisIsThePixel(e.offsetX, e.offsetY)) {
+      canvas.style.cursor = "pointer";
+    }
+    else {
+      canvas.style.cursor = "default";
+    }
+  }
+
+  function nextLevel(e) {
+    if (thisIsThePixel(e.offsetX, e.offsetY)) {
+      makeLevel(canvas, resolution);
+    }
+  }
+
+  canvas.addEventListener('mousemove', changeCursor);
+  canvas.addEventListener('click', nextLevel);
+}
+
+makeGame(canvas, 8);
