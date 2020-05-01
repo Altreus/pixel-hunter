483c218a007dc7eea0997029549a42d1f5a15bfc
Alastair Douglas (altreus) <altreus@altre.us>
2020-05-01 22:08:08 +0100

    Updates or whatever

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById('game-canvas');
-canvas.style.background = "#03fafa";
+//var canvas = document.getElementById('game-canvas');
+//canvas.style.background = "#03fafa";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -85,4 +85,104 @@ function makeGame(canvas, resolution) {
   canvas.addEventListener('click', nextLevel);
 }
 
-makeGame(canvas, 8);
+//makeGame(canvas, 8);
+
+class Game {
+  constructor(targetElement) {
+    this.container = targetElement;
+    var newGameButton = document.createElement('button');
+    newGameButton.classList.add('new-game');
+    newGameButton.innerHTML = "START";
+
+    newGameButton.addEventListener('click', function() {
+      this.classList.add('hidden');
+      (new Level(0, this)).start();
+    });
+
+    this.container.appendChild(newGameButton);
+  }
+
+  // These construct callbacks
+  changeCursor(level) {
+    var self = this;
+
+    return function(e) {
+    }
+  }
+}
+
+class Level {
+  // This can become an algorithm later
+  static levelParams(l) {
+    var levels = [
+      { h: 50, w: 20, s: 15, ai: null },
+      { h: 50, w: 30, s: 15, ai: null },
+      { h: 50, w: 35, s: 10, ai: null },
+      { h: 50, w: 40, s: 10, ai: null },
+      { h: 50, w: 50, s: 8,  ai: null },
+    ];
+  }
+
+  constructor(level, game) {
+    this.level = level;
+    this.game = game;
+
+    var params = Level.levelParams(level);
+    var canvas = document.createElement('canvas');
+    canvas.height = params.h * params.s;
+    canvas.width = params.w * params.s;
+
+    game.container.appendChild(canvas);
+
+    canvas.addEventListener('mousemove', game.changeCursor(this));
+    canvas.addEventListener('mousemove', this.highlightMousePixel());
+    canvas.addEventListener('click',     game.nextLevel(this));
+
+    this.goalPixel = [Math.floor(Math.rand(params.w)), Math.floor(Math.rand(params.h))];
+    this.params = params;
+  }
+
+  targettedPixel(e) {
+    return px2grid(e.offsetX, e.offsetY);
+  }
+
+  px2grid(x,y) {
+    return [Math.floor(x / this.params.s), Math.floor(y / this.params.s)];
+  }
+
+  grid2px(x,y) {
+    return [x * this.params.s, y * this.params.s];
+  }
+
+  highlightMousePixel() {
+    var self = this;
+    return function(e) {
+      self.clear();
+      self.drawRect(e.offsetX, e.offsetY, 1, 1, 'rgb(30,200,30)');
+    }
+  }
+
+  clear() {
+    var ctx = this.canvas.getContext('2d');
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+  }
+
+  clearRect(x, y, w=1, h=1, fillStyle) {
+    var ctx = this.canvas.getContext('2d');
+    var rect = [...this.grid2px(x,y), ...this.grid2px(w,h)];
+
+    this.canvas.getContext('2d').clearRect(...rect);
+  }
+
+  drawRect(x, y, w=1, h=1, fillStyle) {
+    var res = this.params.s;
+    var ctx = this.canvas.getContext('2d');
+    var rect = [...this.grid2px(x,y), ...this.grid2px(w,h)];
+
+    ctx.fillStyle = fillStyle;
+    ctx.fillRect(...rect);
+  }
+}
+
+window.game = new Game(document.body);
