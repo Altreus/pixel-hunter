f66ca322cafc4dcc00cc04e9399ed855eb15b610
Alastair Douglas (altreus) <altreus@altre.us>
2020-05-28 22:38:56 +0100

    Replace button overrides with handlers

--- ui/button.lua
+++ ui/button.lua
@@ -2,22 +2,15 @@ local setCursor = require 'ui.cursor'
 local Image = require 'ui.image'
 local Button = Image:extends()
 
-function Button:onMouseOut(mousePoint)
-    __D("Mouse out " .. self.__localname__)
-    setCursor()
-    Button.super.onMouseOut(self, mousePoint)
-end
+function Button:new(...)
+    Button.super.new(self, unpack({...}))
+    self:addHandler('mouseover', function()
+        setCursor(love.mouse.getSystemCursor('hand'))
+    end)
 
-function Button:onMouseOver(mousePoint)
-    __D("Mouse over " .. self.__localname__)
-    if not self:isVisible() then
-        __D("... but invisible")
-        return
-    end
-
-    local cursor = love.mouse.getSystemCursor('hand')
-    setCursor(cursor)
-    Button.super.onMouseOver(self, mousePoint)
+    self:addHandler('mouseout', function()
+        setCursor(nil)
+    end)
 end
 
 function Button:contains(vec)
