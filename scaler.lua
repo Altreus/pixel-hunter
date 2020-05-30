34a206d654ddddd30f82810f6ada19396e5ad1e4
Alastair Douglas (altreus) <altreus@altre.us>
2020-05-30 21:29:01 +0100

    Remove cruft from scaler

--- scaler.lua
+++ scaler.lua
@@ -1,5 +1,3 @@
-local geo = require('geometry')
-
 -- Returns either the X ratio or the Y ratio of the two vectors, whichever is
 -- smaller. Use this to determine how much to scale a rect in order to match
 -- another one, by passing their diagonals as vectors.
@@ -8,19 +6,4 @@ function scaleFactor(vec1, vec2)
     local heightFactor = vec2:getY() / vec1:getY()
 
     return math.min(widthFactor, heightFactor)
-end
-
--- Returns coordinates of vec1 centred in vec2, relative to rect2
-function centreRect(vec1, vec2)
-    return geo.Vec(
-        (vec2:getX() - vec1:getX())  / 2,
-        (vec2:getY() - vec1:getY()) / 2
-    )
-end
-
-function scaleToWindow(rect)
-    local windowRect = geo.Rect(love.graphics.getWidth(), love.graphics.getHeight())
-
-    local k = scaleFactor(rect, windowRect);
-    return rect:scaled(k)
 end
