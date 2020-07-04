f19ee8416bd3f54ee2124c02e769b14160ae31db
Alastair Douglas (altreus) <altreus@altre.us>
2020-07-04 19:05:29 +0100

    Export text UI item

--- ui/init.lua
+++ ui/init.lua
@@ -3,6 +3,7 @@ ui.Drawable = require 'ui.drawable'
 ui.Button = require 'ui.button'
 ui.Pane = require 'ui.pane'
 ui.Image = require 'ui.image'
+ui.Text = require 'ui.text'
 
 ui.setCursor = require 'ui.cursor'
 
